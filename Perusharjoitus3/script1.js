80b39d5f6427e8e8fe66b27f6e9ea70a4af114e9
Jarmo6565 <pjarmo75@gmail.com>
2021-04-10 14:57:33 +0300

    En keksi tähän mitään

--- Perusharjoitus3/script1.js
+++ Perusharjoitus3/script1.js
@@ -6,9 +6,10 @@ Function muutaSalaSana3()
       var teksti1 = "";
       var salasana1 = document.getElementById('salasana').value
 
-      for(x = 0; x > salasana1.length; x++)
+      for(x = 0; x > salasana1.length; x++);
       {
-        teksti3 += salasana1[x] + "Jarmo";
+        teksti3 += salasana1[x] + "Ö";
       }
       dokument.getElementById('vastaus1').inneHTML = teksti1;
     }
+}
