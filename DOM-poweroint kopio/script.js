ffb9015317a4329acc747d6140b2ca89c4762c97
Jarmo6565 <pjarmo75@gmail.com>
2021-04-14 18:35:15 +0300

    Dom laitettu toimivaksi, kivi_paperi_sakset_ei toimi oikein  14,4,2021 klo18.35

--- DOM-poweroint kopio/script.js
+++ DOM-poweroint kopio/script.js
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-<script src="testi.js"></script>
-</head>
-<body>
-<form class="" action="index.html" method="post">
-<input id="nimi" placeholder="Anna tähän nimesi">
-<button type="button" onclick="toimi()">Paina Minua</button>
-</form>
-</body>
-</html>
+
 
 function toimi()
 {
   var nimi = document.getElementById('nimi').value;
-  alert("Hei " + nimi + "Onpas sinulla todella komea nimi!");
-  
+  alert("Hei " + nimi + "!" + " Onpas sinulla todella komea nimi!");
 }
