5b7f0a2abec49b4b15905f4557c769b951ff19bb
Jarmo6565 <pjarmo75@gmail.com>
2021-05-05 12:18:38 +0300

    Switch-case_ja taulukko tehtävät kaikki 4 valmiit.

--- Switch-case_ja taulukko_tehtava/script_3.js
+++ Switch-case_ja taulukko_tehtava/script_3.js
@@ -9,7 +9,7 @@ function haeNumerot(){
   var tulostus = "<table border = '1'>";
 
  //tässä luetellan akioiden lukumäärät
-	for (var x = 0 ; x < 5 ; x++){
+	for (var x = 0 ; x < 4 ; x++){
 
   //tässä kerrotaan monta ruudukkoa tehdään alkioille
   //" montako huonetta kuuluu tähän"
@@ -27,112 +27,5 @@ function haeNumerot(){
       tulostus += "</table>";
 
 
-      document.getElementById("tulostus").innerHTML = tulostus;
+      document.getElementById("taulukko").innerHTML = tulostus;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function haeNumerot(){
-  var numerot = "";
-	var  taulukko1 = [1, 2, 1, 24];
-  var  taulukko1 = [1, 2, 1, 24];
-  var  taulukko2 = [8, 11, 9, 4];
-  var  taulukko3 = [7, 0, 7, 27];
-  var  taulukko4 = [7, 4, 28, 14];
-  var  taulukko5 = [3, 10, 26, 7];
-
-	for (var i=0 ; i < 5 ; i++){
-  };
-
-  console.log(taulukko1);
-  console.log(taulukko2);
-  console.log(taulukko3);
-  console.log(taulukko4);
-  console.log(taulukko5);
-
- //document.getElementById("numerot").value = numerot;
- document.write(numerot);
-
-//window.alert(taulukko1);
-//window.alert(taulukko2);
-//window.alert(taulukko3);
-//window.alert(taulukko4);
-//window.alert(taulukko5);
-}
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//functio tukko()
-//    {
-//var  taulukko1 = [1, 2, 1, 24];
-//var  taulukko3 = [7, 0, 7, 27];
-//var  taulukko4 = [7, 4, 28, 14];
-//var  taulukko5 = [3, 10, 26, 7];
-//int [][] taulukko1 = [[1, 2, 1, 24][8, 11, 9, 4][7, 0, 7, 27][7, 4, 28, 14][3, 10, 26, 7]];
-
-
-/*
-function haeNumerot(){
-  var numerot = [];
-	var  taulukko1 = [1, 2, 1, 24];
-  var  taulukko2 = [8, 11, 9, 4];
-  var  taulukko3 = [7, 0, 7, 27];
-  var  taulukko4 = [7, 4, 28, 14];
-  var  taulukko5 = [3, 10, 26, 7];
-
-	for (var i=0 ; i < 5 ; i++){
-  };
-
-  console.log(taulukko1);
-  console.log(taulukko2);
-  console.log(taulukko3);
-  console.log(taulukko4);
-  console.log(taulukko5);
-
-  document.getElementById("numerot").innerHTML = numerot;
-
-//window.alert(taulukko1);
-//window.alert(taulukko2);
-//window.alert(taulukko3);
-//window.alert(taulukko4);
-//window.alert(taulukko5);
-}
-
-//<input type="button" value="Ilmoita numerot" onClick="haeNumerot();" />
-*/
--- Switch-case_ja taulukko_tehtava/script_4.js
+++ Switch-case_ja taulukko_tehtava/script_4.js
@@ -1,7 +1,29 @@
-let tulo;
-        for (let i = 1; i<=52; i++) {
-            for (let j = 1; j<=4; j++) {
-                tulo = i*j;
-                console.log(i + ' kertaa ' + j + ' on ' + tulo + ".");
-            }
-        }
+function arvoKortit()
+{
+  var korttipakka = [''];
+  var maat = ['pata','risti','hertta','ruutu'];
+  var numerot = ['2','3','4','5','6','7','8','9','10','jätkä','akka','Kuningas','Ässä'];
+  laskuri = 0;
+  for(m = 0; m < maat.length; m++)
+  {
+    for(n = 0; n < numerot.length; n++)
+    {
+      if(m == maat.length-1 && n == numerot.length-1)
+      {
+        korttipakka[laskuri] = maat[m] + numerot[n];
+      }
+      else
+      {
+        korttipakka[laskuri] =  maat[m]+numerot[n];
+        laskuri++;
+      }
+    }
+  }
+  var kortit = [];
+  for(l = 0; l < 5; l++)
+  {
+    temp = korttipakka[Math.floor(Math.random()*52)];
+    kortit[l] = temp;
+  }
+  document.getElementById('parttikokka').innerHTML = '<p>'+kortit+'</p>';
+}
